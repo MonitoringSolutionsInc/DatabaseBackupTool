df762f9611b9c94cc65c580255100fcae81e029b
JazzGlobal <waymer147@gmail.com>
2020-06-11 09:09:01 -0400

    Renamed MainForm to MainFormTests for clarity.

---
+++ DatabaseBackupTools.UnitTests/MainFormTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseBackupTools.UnitTests
+{
+    [TestClass]
+    public class MainForm
+    {
+        [TestMethod]
+        public void GetDatabases_ListIsNotEmpty_ReturnsNonNullList()
+        {
+            // Arrange
+            var mainform = new DatabaseBackupTool.MainForm();
+            mainform.InitializeConnection();
+            // Act
+            var result = mainform.GetDatabases();
+
+
+            // Assert
+            Assert.IsTrue(result != null);
+        }
+        [TestMethod]
+        public void GetDatabases_ListIsEmpty_ReturnsNonNullList() /* This tests the method's catch block. 
+                                                                    We do not call mainform.InitializeConnection(), 
+                                                                    therefore it cannot connect to the database. */
+        {
+            // Arrange
+            var mainform = new DatabaseBackupTool.MainForm();
+
+            // Act
+            var result = mainform.GetDatabases();
+
+            // Assert
+            Assert.IsTrue(result != null);
+        }
+    }
+}
