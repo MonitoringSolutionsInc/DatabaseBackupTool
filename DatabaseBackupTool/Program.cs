fa330ac581f427e586dcda13179c834472b0c8ed
JazzGlobal <waymer147@gmail.com>
2021-04-13 14:53:17 -0400

    Revert "Added Connection form"
    This reverts commit e8a3f96e207560c1b4a978e53b53e478c975cac2.

--- DatabaseBackupTool/Program.cs
+++ DatabaseBackupTool/Program.cs
@@ -16,7 +16,7 @@ namespace DatabaseBackupTool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Connection());
+            Application.Run(new Dashboard());
         }
     }
 }
