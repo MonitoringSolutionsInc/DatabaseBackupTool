e8b78dd3ad08a9f230b689a63c7320c9594e33c3
J Rick <runesword85@yahoo.com>
2021-04-14 13:16:19 -0400

    Pressing Enter now clicks Connect

--- DatabaseBackupTool/Connection.Designer.cs
+++ DatabaseBackupTool/Connection.Designer.cs
@@ -135,6 +135,7 @@ namespace DatabaseBackupTool
             // 
             // Connection
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(232, 154);
