ecc6de66bba71ba22df72d0d2260d9d6d963625f
JazzGlobal <waymer147@gmail.com>
2021-05-20 11:06:10 -0400

    loadSqlConnection is now a public static function named LoadSqlConnectionXml.

--- DatabaseBackupTool/Dashboard.cs
+++ DatabaseBackupTool/Dashboard.cs
@@ -7,28 +7,28 @@ namespace DatabaseBackupTool
     public partial class Dashboard : Form
     {
         public static SqlConnectorInfo.SqlConnectionInfoData SqlInfoData; // Globally accessible instance of the XML loaded SQL Connection Info.
-        private string xmlPath = "SqlConnectorData.xml";
+        private static string xmlPath = "SqlConnectorData.xml";
         public Dashboard()
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            loadSqlConnectorXml();
+            LoadSqlConnectionXml();
         }
 
-        private void loadSqlConnectorXml()
+        public static void LoadSqlConnectionXml()
         {
             SqlInfoData = SqlConnectorInfo.LoadSqlConnectionData(xmlPath);
         }
         private void databaseBackupToolBtn_Click(object sender, EventArgs e)
         {
-            loadSqlConnectorXml();
+            LoadSqlConnectionXml();
             Backup backup = new Backup();
             backup.ShowDialog();
         }
 
         private void restoreBackupToolBtn_Click(object sender, EventArgs e)
         {
-            loadSqlConnectorXml();
+            LoadSqlConnectionXml();
             Restore restore = new Restore();
             restore.ShowDialog();
         }
