25006ecb47eedc359475b6310b67b91e57ce74c6
Jerrick Snyder <runesword85@yahoo.com>
2021-06-23 14:34:06 -0400

    AddMoreLogging

--- DatabaseBackupTool/Restore.cs
+++ DatabaseBackupTool/Restore.cs
@@ -96,7 +96,7 @@ namespace DatabaseBackupTool
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-            BG_DoWork(worker);
+            BG_DoWork(worker, "BG1");
         }
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
@@ -111,7 +111,7 @@ namespace DatabaseBackupTool
         private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-            BG_DoWork(worker);           
+            BG_DoWork(worker, "BG3");           
         }
         private void backgroundWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
@@ -127,7 +127,7 @@ namespace DatabaseBackupTool
         private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-            BG_DoWork(worker);
+            BG_DoWork(worker, "BG4");
         }
         private void backgroundWorker4_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
@@ -143,7 +143,7 @@ namespace DatabaseBackupTool
         private void backgroundWorker5_DoWork(object sender, DoWorkEventArgs e)
         {
             BackgroundWorker worker = sender as BackgroundWorker;
-            BG_DoWork(worker);
+            BG_DoWork(worker, "BG5");
         }
         
         private void backgroundWorker5_ProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -180,17 +180,17 @@ namespace DatabaseBackupTool
             recursiveBox.Enabled = true;
         }
 
-        private void BG_DoWork(BackgroundWorker worker)
+        private void BG_DoWork(BackgroundWorker worker, String name)
         {
             SQLConnector conn = connectToSQL(); //create connection outside of loop for more reliable operation
-
+            
             while (i < filesToRestore.Length)
             {
                 lock (key)
                     i++;
                 if (i < filesToRestore.Length) //perform this check here instead of inside lock to save on lock time
                 {
-                    restoreDatabase(i, filesToRestore, conn);
+                    restoreDatabase(i, filesToRestore, conn, name);
                     int percentComplete = (int)((float)i / (float)(filesToRestore.Length) * 100);
                     worker.ReportProgress(percentComplete);
                 }
@@ -205,15 +205,18 @@ namespace DatabaseBackupTool
         }
 
 
-        private void restoreDatabase(int i, string[] filesToRestore, SQLConnector conn)
+        private void restoreDatabase(int i, string[] filesToRestore, SQLConnector conn, String workersName)
         {
             string databaseName = filesToRestore[i].Split('\\').Last().Split('.').First();
             string restoreSql = $@"RESTORE DATABASE [{databaseName}] FROM DISK='{filesToRestore[i]}' WITH REPLACE";
-            Console.WriteLine($"Restoring {databaseName} using query: {restoreSql}");
+            Console.WriteLine($"{workersName} runs: Restoring {databaseName} using query: {restoreSql}");
+            Logger.Info($"{workersName} runs: Restoring {databaseName} using query: {restoreSql}");
             try
             {
                 conn.Open();
-                conn.ReadResults(conn.CreateCommand(restoreSql));
+                var command = conn.CreateCommand(restoreSql);
+                command.CommandTimeout = 0;
+                conn.ReadResults(command);
                 conn.Close();
             }
             catch (Exception ex)
@@ -293,7 +296,7 @@ namespace DatabaseBackupTool
                     Console.WriteLine($"{dbName} currently in state: {result}");
                     Console.WriteLine("restoring... starting check over again");
                     Logger.Info($"{dbName} currently in state: {result} - restoring... starting check over again");
-                    restoreDatabase(i, filesToRestore, conn);
+                    restoreDatabase(i, filesToRestore, conn, "Final Check");
                     i = -1; //reset, start check over again to check this one again
                 }
             }
@@ -329,6 +332,7 @@ namespace DatabaseBackupTool
 
 
             Console.WriteLine($"{work}% {who} restored a file in {time.Seconds}.{time.Milliseconds} seconds");
+            Logger.Info($"{work}% {who} restored a file in {time.Seconds}.{time.Milliseconds} seconds");
             progressBar1.Value = work;
             progressBarLabel.Text = $"{work}% Complete";
         }
